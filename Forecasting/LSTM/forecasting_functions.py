8475effeb3bc41d30e266e08ef9565fc5b54bbe0
Stijn Staring <stijn.staring@student.kuleuven.be>
2021-04-03 14:40:09 +0200

    update

--- Forecasting/LSTM/forecasting_functions.py
+++ Forecasting/LSTM/forecasting_functions.py
@@ -1,10 +1,30 @@
 import datetime as dt
 import pandas as pd # pandas
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 import warnings as wn
 wn.filterwarnings(action='ignore')
 from Test_basemodel_functions import EnglandAndWalesHolidayCalendar
+
+
+def figure_layout(figsize=(10,8),titel="",xlabel="",ylabel="",fontsize_titel=18,fontsize_axis=16,fontsize_legend=14,fontsize_ticks=16,grid:bool = False):
+
+    plt.figure(figsize=figsize)
+    ax1 = plt.gca()
+    plt.rc('legend',fontsize=fontsize_legend)
+    plt.title(titel, fontsize=fontsize_titel, fontweight = 'bold')
+    plt.grid(grid)
+    plt.xlabel(xlabel, fontsize=fontsize_axis)
+    plt.ylabel(ylabel, fontsize=fontsize_axis)
+    for tick in ax1.xaxis.get_major_ticks():
+            tick.label1.set_fontsize(fontsize_ticks)
+    #         tick.label1.set_fontweight('bold')
+    for tick in ax1.yaxis.get_major_ticks():
+        tick.label1.set_fontsize(fontsize_ticks)
+    #     tick.label1.set_fontweight('bold')
+
+    return ax1
 
 
 # subsitute the missing values by prev week < prev day < mean of all the values at that time of the day (rarely used)
